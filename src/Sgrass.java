ba258da6edcffca59b4e81d7b3b8856acd6721a0
Tyler Lian <tylerlian@gmail.com>
2019-12-03 23:40:36 -0800

    fixing try 3

--- src/Sgrass.java
+++ src/Sgrass.java
@@ -1,33 +1,20 @@
 import processing.core.PImage;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
 public class Sgrass extends ImmobileEntity{
 
     public Sgrass(String id, Point position, List<PImage> images, int actionPeriod){
         super(id, position, images, 0, "seaGrass", actionPeriod);
     }
 
-<<<<<<< HEAD
+
     public String toString(){
         return "(" + this.getPosition().getX() + ", " + this.getPosition().getY() + ")";
     }
-//    public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
-//        Optional<Point> openPt = world.findOpenAround(this.getPosition());
-//
-//      if (openPt.isPresent())
-//      {
-//         ActivityEntity fish = imageStore.createFish("fish" + this.getId(),
-//                 openPt.get(), 20000 +
-//                         (new Random()).nextInt(30000 - 20000),
-//                 imageStore.getImageList("fish"));
-//         world.addEntity((Entity) fish);
-//         fish.scheduleActions(scheduler, world, imageStore);
-//      }
-//      scheduler.scheduleEvent(this,
-//         this.createActivityAction( world, imageStore),
-//         this.getActionPeriod());
-//    }
-=======
+
     public void executeActivity(WorldModel world, ImageStore imageStore, EventScheduler scheduler) {
         Optional<Point> openPt = world.findOpenAround(this.getPosition());
 
@@ -44,5 +31,4 @@ public class Sgrass extends ImmobileEntity{
          this.createActivityAction( world, imageStore),
          this.getActionPeriod());
     }
->>>>>>> parent of 6da6b3e... fixed trees
 }
